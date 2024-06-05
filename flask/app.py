b292d74bb67ace4d863a6515bf6760eb8b6685fa
user <user@SAE61-0.1.myguest.virtualbox.org>
2024-06-05 23:00:50 +0200

    v0.8

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
-from werkzeug.security import generate_password_hash, check_password_hash
 import re
+import hashlib
 
 app = Flask(__name__)
 
@@ -28,17 +28,18 @@ def inscription():
         mail = Users['email']
         password = Users['mdp']
 
-        # Regex pour les contraintes d'identifiant
-        if not re.match(r'^[a-z]{6,10}$', identifiant):
-            return "L'identifiant doit contenir entre 6 et 10 caractères en minuscules uniquement." 
+        # Regex pour l'identifiant
+        if not re.match(r'^[a-zA-Z]{6,25}$', identifiant):
+            return "L'identifiant doit contenir au moins 6 caractères." 
+		#majuscule, minuscule + 6 à 25 caractères
 
         # Regex pour les contraintes de mail
         if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', mail):
             return "L'adresse e-mail est invalide." 
 
         # Regex pour les contraintes de mot de passe
-        if not re.match(r'^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[#$%{}@])[a-zA-Z0-9#$%{}@]{6,15}$', password): 
-            return "Le mot de passe doit contenir entre 6 et 15 caractères, au moins 1 chiffre, au moins 1 majuscule, au moins 1 minuscule et au moins 1 caractère parmi #%{}@."
+        if not re.match(r'^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[#$%{}@])[a-zA-Z0-9#$%{}@]{6,25}$', password): 
+            return "Le mot de passe doit contenir entre 6 et 25 caractères, avec chiffre, majuscule, minuscule et caractère parmi #%{}@."
 
         cur = mysql.connection.cursor()
 
@@ -54,11 +55,15 @@ def inscription():
             cur.close()
             return "Cette adresse e-mail est déjà utilisée. Veuillez en choisir une autre."
 
-        # Insertion des données sans hashage du mot de passe
-        cur.execute("INSERT INTO Utilisateurs(identifiant, mail, password) VALUES (%s, %s, %s)", (identifiant, mail, password))
+		#hash du mot de passe SHA256
+        hash_mdp = hashlib.sha256(password.encode())
+		
+
+        cur.execute("INSERT INTO Utilisateurs(identifiant, mail, password) VALUES (%s, %s, %s)", (identifiant, mail, hash_mdp.hexdigest()))
+
         mysql.connection.commit()
         cur.close()
-        return "Félicitations ! Vous êtes inscrits."
+        return f"Bienvenue {identifiant}."
     return render_template('inscription.html')
 
 ##############################
@@ -69,15 +74,22 @@ def connexion():
         Users = request.form
         identifiant = Users['identifiant']
         password = Users['mdp']
+
+        #hashage du mdp
+        hash_mdp = hashlib.sha256(password.encode())
+        hash_mdp = hash_mdp.hexdigest()
+
         cur = mysql.connection.cursor()
         cur.execute("SELECT password FROM Utilisateurs WHERE identifiant = %s", (identifiant,))
         user_password = cur.fetchone()
+		
+		
         cur.close()
         if user_password:
-            if user_password[0] == password:
+            if user_password[0] == hash_mdp:
                 return f"Bonjour {identifiant}!"
             else:
-                return "Connexion échouée, vérifiez votre mot de passe"
+                return "Identifiant ou mot de passe incorrect"
         else:
             return "Identifiant ou mot de passe incorrect"
     return render_template('connexion.html')
